9a7e51642b3c163d7c8ed8a4d916e920bad6bfeb
Shannon <shannon.nell92@gmail.com>
2021-06-02 23:51:38 -0400

    Mini update ensuring same as other branch
    Now working on version 2 with new API

---
+++ assets/js/script.js
@@ -0,0 +1,86 @@
+//variables
+var songName = document.querySelector('#song-name');
+var artistName = document.querySelector('#artist-name');
+var albumCover = document.querySelector('#album-cover');
+var playCircle = document.querySelector('.circle');
+var searchEl = document.querySelector('.search-icon');
+var playBtn = document.querySelector('.play-button');
+var spotifyIframe = document.querySelector('.spotifySong');
+var spotifyEl = document.querySelector('.spotify');
+var artistInfo = document.getElementById('#artist-info');
+
+// // This function gets us an access token to use throughout other functions
+// //private post request to get token from spotify
+var getToken = async () => {
+    var clientId = 'ef677111698447b4a02f98e0e528437b';
+    var clientSecret = '0ad72dace42f4cf796c70cb87cec8fec'; 
+    var base64Encode = btoa(clientId + ':' + clientSecret);
+    // console.log(base64Encode);
+    var result = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded',
+            'Authorization' : 'Basic ' + base64Encode
+        },
+        body: 'grant_type=client_credentials'
+    });
+    data = await result.json();
+    console.log(data.access_token);
+    return data.access_token;
+}
+
+//search button clicked
+document.addEventListener('keypress' , function(e) {
+    if (e.key === 'Enter'){
+
+        console.log("Randmon something " + data.access_token);
+        var access = data.access_token;
+        console.log(access);
+        //get value of search box
+        var searchInfo = $('.search-box').val();
+        //accessToken will need to be created as this one expires see below apiController for potential solution
+        // console.log(accessToken);
+        
+        //Make spotify API call using track endpoint
+        $.ajax({
+            url: `https://api.spotify.com/v1/search?q=${searchInfo}&type=track`,
+            type: 'GET',
+            headers: {
+                'Authorization' : 'Bearer ' + access
+            },
+            //if call was successful, show data
+            success: function(data) {
+                //load songs from Spotify onto page
+                console.log(data);
+                //load songs from Spotify
+                loadSong(data);
+            }
+        })        
+    }
+});
+
+//trackInfo is data from search button click
+//this function loads the song information at bottom right corner
+function loadSong(trackInfo) {
+    //song id
+    var id = trackInfo.tracks.items[0].id;
+    //when song is loaded, load play button on visualizer
+    //spotify Player
+    spotifyIframe.style.display = 'block';
+    spotifyIframe.setAttribute('src', `https://open.spotify.com/embed/track/${id}`);
+
+    //Artist information appears from Wiki API
+    // artistInfo
+
+};
+
+function getWiki(trackInfo) {
+    var searchInfo = $('.search-box').val();
+    console.log(searchInfo);
+
+    var wikiKey = '';
+    var wikiURL = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchInfo}`;
+
+}
+
+getToken();
