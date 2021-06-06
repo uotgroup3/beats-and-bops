af3453dea6a7579a99c38b494f9cb38a29690e11
lisahuele <lisahuele@gmail.com>
2021-06-06 12:37:37 -0400

    Sample v3.js
    Created a potential sample of music visualizer with bars.
    
    Created 2nd js folder to sample sphere using audio js

---
+++ assets/js/v3.2.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    //set size of device
+    canvas=document.getElementById("visualizer");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx=canvas.getContext("2d");
+}
--- assets/js/v3.js
+++ assets/js/v3.js
@@ -1,15 +1,63 @@
-function handleFiles(event) {
-    var files = event.target.files;
-    $("#src").attr("src", URL.createObjectURL(files[0]));
-    document.getElementById("audio").load();
-}
+window.onload = function() {
+  
+  var file = document.getElementById("upload");
+  var audio = document.getElementById("audio");
+  
+  file.onchange = function() {
+    var files = this.files;
+    audio.src = URL.createObjectURL(files[0]);
+    audio.load();
+    audio.play();
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
 
-document.getElementById("upload").addEventListener("change", handleFiles, false);
+    var canvas = document.getElementById("visualizer");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var ctx = canvas.getContext("2d");
 
-window.onload = () =>{
-    var visualizer = new Visualiser();
-    
-    window.onresize = () => {
-      visualizer.resize();
+    src.connect(analyser);
+    analyser.connect(context.destination);
+
+    analyser.fftSize = 256;
+
+    var bufferLength = analyser.frequencyBinCount;
+
+    var dataArray = new Uint8Array(bufferLength);
+
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+
+    function renderFrame() {
+      requestAnimationFrame(renderFrame);
+
+      x = 0;
+
+      analyser.getByteFrequencyData(dataArray);
+
+      ctx.fillStyle = "transparent";
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      for (var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        
+        var r = barHeight + (30 * (i/bufferLength));
+        var g = 250 * (i/bufferLength);
+        var b = 50;
+
+        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+        x += barWidth + 3;
+      }
     }
-  }
+
+    audio.play();
+    renderFrame();
+  };
+};
